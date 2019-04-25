4e5ad75ac9519fb8b09d07a3d3407cf80950c6be
goandreus <andreshtml5@gmail.com>
2019-04-25 09:10:17 -0500

    add

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -95,7 +95,7 @@ class CardPage extends StatelessWidget {
             color: Colors.black26,
             blurRadius: 10.0,
             spreadRadius: 2.0,
-            offset: Offset(3.0, 10.0)
+            offset: Offset(2.0, 10.0)
           )
         ]
       ),
