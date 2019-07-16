c45a6e40d1e9cd19f70b2de2c6bdb864046e8d36
goandreus <andreshtml5@gmail.com>
2019-07-16 00:53:31 -0500

    update

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:componentes/src/pages/home_temp.dart';
+import 'package:flutter/material.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Componentes'),
+        ),
+        body: HomePageTemp()
+      ),
+    );
+  }
+}
