9b7c5a7482753daf0f8323125fed1062367a105e
goandreus <andreshtml5@gmail.com>
2019-08-06 12:45:17 -0500

    JSONString - map

--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -1,15 +1,21 @@
+import 'dart:convert';
+
 import 'package:flutter/services.dart' show rootBundle;
 
 class _MenuProvider{
   List<dynamic> opciones = [];
 
   _MenuProvider(){
-    cargarData();
+    
   }
 
-  cargarData(){
-    rootBundle.loadString('data/menu_opts.json').then((data){
-      print(data);
+   Future<List<dynamic>> cargarData() async {
+    final resp = await rootBundle.loadString('data/menu_opts.json').then((data){
+      Map dataMap = json.decode(data);
+      print(dataMap['rutas']);
+      opciones = dataMap [''];
+
+      return opciones;
     });
   }
 }
