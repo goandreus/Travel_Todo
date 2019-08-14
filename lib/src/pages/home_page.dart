7a856eced215740d22f7834e6d79f6a56b977db2
goandreus <andreshtml5@gmail.com>
2019-08-14 10:09:13 -0500

    navigator

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/providers/menu_provider.dart';
 import 'package:componentes/src/utils/icon_string_util.dart';
 import 'package:flutter/material.dart';
@@ -19,16 +20,14 @@ class HomePage extends StatelessWidget {
       initialData: [],
       builder: (context, AsyncSnapshot<List<dynamic>> snapshot){
         return ListView(
-        children: _ListaItems(snapshot.data),
+        children: _ListaItems(snapshot.data, context),
       );
       },
     );
 
-    
-
   }
 
-    List<Widget> _ListaItems(List<dynamic> data){
+    List<Widget> _ListaItems(List<dynamic> data, BuildContext context){
       final List<Widget> opciones = [];
       data.forEach((opt){
         final widgetTemp = ListTile(
@@ -36,7 +35,12 @@ class HomePage extends StatelessWidget {
           leading: getIcon(opt['icon']),
           trailing: Icon(Icons.keyboard_arrow_right, color: Colors.black),
           onTap: (){
-
+            final route = MaterialPageRoute(
+              builder: (context){
+                return AlertPage();
+              }
+            );
+            Navigator.push(context, route);
           },
         );
         opciones..add(widgetTemp)
