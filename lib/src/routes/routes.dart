7b9344f94c2d14de4d7351ea66ad63737ce556ba
goandreus <andreshtml5@gmail.com>
2019-08-19 22:12:04 -0500

    Feature_card_page

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,7 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
         '/' : (BuildContext context) => HomePage(),
         'alert' : (BuildContext context) => AlertPage(),
         'avatar' : (BuildContext context) => AvatarPage(),
+        'card' : (BuildContext context) => CardPage(),
 };
 
 }
