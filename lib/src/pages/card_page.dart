e2fce76db9503f791c706986a33eaaf087de0c04
goandreus <andreshtml5@gmail.com>
2019-08-19 22:30:44 -0500

    card

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -7,6 +7,36 @@ class CardPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Cards"),
       ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          _cardTipo1()
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo1(){
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.photo_album, color: Colors.blue,),
+            title: Text('lorem lorem lorem'),
+            subtitle: Text("lorem lorem lorem lorem lorem lorem"),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+            FlatButton(child: Text('Cancelar'),
+            onPressed: (){},
+            ),
+            FlatButton(child: Text('ok'),
+            onPressed: (){},
+            ),
+          ],)
+        ],
+      ),
     );
   }
 }
