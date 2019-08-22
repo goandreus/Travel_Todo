cb53316e2a3a03a1264c0a30f66702538675ae2c
goandreus <andreshtml5@gmail.com>
2019-08-22 10:59:57 -0500

    Container and  Animated Container

---
+++ lib/src/pages/animated_container.dart
@@ -0,0 +1,56 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+
+class AnimatedContainerPage extends StatefulWidget {
+  @override
+  _AnimatedContainerPageState createState() => _AnimatedContainerPageState();
+}
+
+class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
+
+  double _width = 50.0;
+  double _height = 50.0;
+  Color _color = Colors.pink;
+
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animated'),
+      ),
+      body: Center(
+        child: Container(
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(
+            borderRadius: _borderRadius,
+            color: _color
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.play_arrow),
+        onPressed: _cambiarForma,
+      ),
+      
+    );
+  }
+  void _cambiarForma(){
+    
+    final random = Random();
+
+    setState(() {
+      _width = random.nextInt(300).toDouble();
+      _height = random.nextInt(300).toDouble();
+      _color = Color.fromRGBO(
+          random.nextInt(255),
+          random.nextInt(255),
+          random.nextInt(255),
+          1);  
+    });
+  }
+}
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
@@ -11,6 +12,7 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
         'alert' : (BuildContext context) => AlertPage(),
         'avatar' : (BuildContext context) => AvatarPage(),
         'card' : (BuildContext context) => CardPage(),
+        'animatedContainer' : (BuildContext context) => AnimatedContainerPage(),
 };
 
 }
