9d0a90af8e8effa3e2739e3c1cae84c76621b3d8
goandreus <andreshtml5@gmail.com>
2019-08-25 11:11:33 -0500

    email and password fields

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -7,7 +7,8 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
 
-  String _nombre;
+  String _nombre = '';
+  String _email = '';
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +21,10 @@ class _InputPageState extends State<InputPage> {
         children: <Widget>[
           Divider(),
           _crearInput(),
+          Divider(),
+          _crearEmail(),
+          Divider(),
+          _crearPassword(),
           Divider(),
           _crearPersona(),
         ],
@@ -55,14 +60,49 @@ class _InputPageState extends State<InputPage> {
 
 
 
+  Widget _crearPassword(){
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        hintText: 'Password',
+        labelText: 'Password',
+        suffixIcon: Icon(Icons.lock_open),
+        icon: Icon(Icons.lock)
+      ),
+      onChanged: (valor) => setState(() {
+            _email = valor;
+        })
+      
+    ); 
+  }
 
 
-
-
+  Widget _crearEmail (){
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        hintText: 'Email',
+        labelText: 'Email',
+        suffixIcon: Icon(Icons.alternate_email),
+        icon: Icon(Icons.email)
+      ),
+      onChanged: (valor) => setState(() {
+            _email = valor;
+        })
+      
+    ); 
+  }
 
   Widget _crearPersona (){
     return ListTile(
       title: Text('Nombre es: $_nombre'),
+      subtitle: Text('Email: $_email'),
     );
   }
 }
