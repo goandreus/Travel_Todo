9b7f8ad370087fb5e896c28656bfea2fe65f8cd4
goandreus <andreshtml5@gmail.com>
2019-08-25 09:50:06 -0500

    inputFields

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -6,6 +6,9 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+
+  String _nombre;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,8 +16,12 @@ class _InputPageState extends State<InputPage> {
         title: Text('Inputs texto'), 
       ),
       body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 2),
         children: <Widget>[
+          Divider(),
           _crearInput(),
+          Divider(),
+          _crearPersona(),
         ],
       ),
     );
@@ -23,7 +30,39 @@ class _InputPageState extends State<InputPage> {
 
   Widget _crearInput(){
     return TextField(
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        counter: Text('Letras ${_nombre.length}'),
+        hintText: 'Nombre de la persona',
+        labelText: 'Nombre',
+        helperText: 'Solo es el nombre',
+        suffixIcon: Icon(Icons.accessibility),
+        icon: Icon(Icons.account_circle)
+      ),
+      onChanged: (valor){
+        setState(() {
+            _nombre = valor;
+        });
+      },
+    );
+  }
 
+
+
+
+
+
+
+
+
+
+
+  Widget _crearPersona (){
+    return ListTile(
+      title: Text('Nombre es: $_nombre'),
     );
   }
 }
