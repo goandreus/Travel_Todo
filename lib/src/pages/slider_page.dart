56293e0b8418108e15f59ff0d3f738523812a9f6
goandreus <andreshtml5@gmail.com>
2019-08-26 08:08:09 -0500

    listview and switch

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -8,6 +8,8 @@ class SliderPage extends StatefulWidget {
 class _SliderPageState extends State<SliderPage> {
 
   double _valorSlider = 100.0;
+  bool _bloquearCheck = true;
+  bool _bloquearSwitch = true;
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +22,8 @@ class _SliderPageState extends State<SliderPage> {
         child: Column(
           children: <Widget>[
             _crearSlider(),
+            _checkBox(),
+            _crearSwitch(),
             Expanded(
               child: _crearImagen()
             ),
@@ -37,7 +41,7 @@ class _SliderPageState extends State<SliderPage> {
       value: _valorSlider,
       min: 10.0,
       max: 400.0,
-      onChanged: ( valor ){
+      onChanged: (_bloquearCheck  ) ? null : ( valor ){
 
         setState(() {
            _valorSlider = valor;
@@ -52,6 +56,40 @@ class _SliderPageState extends State<SliderPage> {
       image: NetworkImage('https://scontent.flim16-3.fna.fbcdn.net/v/t1.0-9/67294457_2296673993719000_6679508060425158656_n.jpg?_nc_cat=104&_nc_oc=AQliepHAYUNdqJ7_wZGcE3gZT5rOvO3Zsue1fXD6r_43IkKwGSeofxseLtFybG8gNdA&_nc_ht=scontent.flim16-3.fna&oh=8c7d37b0307a82858731b9c61e1503d8&oe=5DCD4843'),
       width: _valorSlider,
       fit: BoxFit.contain,
+    );
+  }
+
+  Widget _checkBox(){
+    //return Checkbox(//
+ //     value: _bloquearCheck,
+ //     onChanged:  (valor){
+ //       setState(() {
+  //         _bloquearCheck = valor ;
+ //       });
+ //     },
+  //  );
+
+
+    return CheckboxListTile(
+      title: Text('Bloquear Slider'),
+      value: _bloquearCheck,
+      onChanged: (valor){
+        setState(() {
+         _bloquearCheck = valor ; 
+        });
+      },
+    ); 
+  }
+
+  Widget _crearSwitch(){
+    return SwitchListTile(
+      title: Text('Bloquear Slider'),
+      value: _bloquearSwitch,
+      onChanged: (valor){
+        setState(() {
+          _bloquearSwitch = valor ;
+        });
+      },
     );
   }
 }
